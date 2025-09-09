a7d6b7563ff9512d8ad84d01dde5b626d01a10c1
NicolasEdier <ediernicolas@gmail.com>
2025-09-09 03:16:47 +0000

    Saving our two init files

---
+++ scripts/init_git_creds.sh
@@ -0,0 +1,11 @@
+!#/usr/bin/bash
+
+USER=ediernicolas@gmail.com
+NAME=NicolasEdier
+
+git config --global --list
+
+git config --global user.email ${USER}
+git config --global user.name ${NAME}
+
+git config --global --list
