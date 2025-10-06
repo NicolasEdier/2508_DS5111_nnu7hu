e3c838cec0d64fb1598b7f2fa37bd90638aef0c9
NicolasEdier <160418304+NicolasEdier@users.noreply.github.com>
2025-10-06 09:58:40 -0400

    Update test_smoketests.py

--- tests/test_smoketests.py
+++ tests/test_smoketests.py
@@ -14,8 +14,7 @@ sys.path.append('.')
 import bin.normalize as nm
 
 def test_python_version():
-	response = nm.python_version()
-	assert response == "3.12.3"
+	312 313
 
 def test_os():
 	response = nm.os_system()
