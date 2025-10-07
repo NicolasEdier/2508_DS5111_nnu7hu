370b8c26110127381059a4ec6486c4e4436e84c5
NicolasEdier <ediernicolas@gmail.com>
2025-10-07 21:55:08 +0000

    Updating cron jobs

--- data-collection-cron-jobs.sh
+++ data-collection-cron-jobs.sh
@@ -1,1 +1,7 @@
-cd home/ubuntu/2508_DS5111_nnu7hu/; make clean; make ygainers_tstamp; make wsjgainer_tstamp 
+30 14 * * 1-5 cd 2508_DS5111_nnu7hu/; make clean; make ygainers_tstamp; make clean
+0 18 * * 1-5 cd 2508_DS5111_nnu7hu/; make clean; make ygainers_tstamp; make clean
+30 21 * * 1-5 cd 2508_DS5111_nnu7hu/; make clean; make ygainers_tstamp; make clean
+
+30 14 * * 1-5 cd 2508_DS5111_nnu7hu/; make clean; make wsjgainers_tstamp; make clean
+0 18 * * 1-5 cd 2508_DS5111_nnu7hu/; make clean; make wsjgainers_tstamp; make clean
+30 21 * * 1-5 cd 2508_DS5111_nnu7hu/; make clean; make wsjgainers_tstamp; make clean
