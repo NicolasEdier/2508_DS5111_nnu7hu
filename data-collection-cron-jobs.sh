62380250ac8e12c696730adf7b562264c7e9750f
NicolasEdier <ediernicolas@gmail.com>
2025-10-09 13:39:20 +0000

    Cron job timing was off by 1 hour

--- data-collection-cron-jobs.sh
+++ data-collection-cron-jobs.sh
@@ -1,7 +1,7 @@
-30 14 * * 1-5 cd 2508_DS5111_nnu7hu/; make ygainers_clean; make ygainers_tstamp; make ygainers_clean
-0 18 * * 1-5 cd 2508_DS5111_nnu7hu/; make ygainers_clean; make ygainers_tstamp; make ygainers_clean
-30 21 * * 1-5 cd 2508_DS5111_nnu7hu/; make ygainers_clean; make ygainers_tstamp; make ygainers_clean
+30 13 * * 1-5 cd 2508_DS5111_nnu7hu/; make ygainers_clean; make ygainers_tstamp; make ygainers_clean
+0 17 * * 1-5 cd 2508_DS5111_nnu7hu/; make ygainers_clean; make ygainers_tstamp; make ygainers_clean
+30 20 * * 1-5 cd 2508_DS5111_nnu7hu/; make ygainers_clean; make ygainers_tstamp; make ygainers_clean
 
-30 14 * * 1-5 cd 2508_DS5111_nnu7hu/; make wsjgainers_clean; make wsjgainers_tstamp; make wsjgainers_clean
-0 18 * * 1-5 cd 2508_DS5111_nnu7hu/; make wsjgainers_clean; make wsjgainers_tstamp; make wsjgainers_clean
-30 21 * * 1-5 cd 2508_DS5111_nnu7hu/; make wsjgainers_clean; make wsjgainers_tstamp; make wsjgainers_clean
+30 13 * * 1-5 cd 2508_DS5111_nnu7hu/; make wsjgainers_clean; make wsjgainers_tstamp; make wsjgainers_clean
+0 17 * * 1-5 cd 2508_DS5111_nnu7hu/; make wsjgainers_clean; make wsjgainers_tstamp; make wsjgainers_clean
+30 20 * * 1-5 cd 2508_DS5111_nnu7hu/; make wsjgainers_clean; make wsjgainers_tstamp; make wsjgainers_clean
